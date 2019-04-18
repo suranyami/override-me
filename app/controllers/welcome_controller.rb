baed72ceff58b87099207f18560f4ceac8612e04
David Parry <david.parry@suranyami.com>
2019-04-18 15:04:14 +1000

    Fix rubocop suggestion
    This does look more succinct, I must admit. :-).

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,17 +6,8 @@ class WelcomeController < ApplicationController
   private
 
   def set_defaults
-    @cronuts_url = if allowable[:cronuts_url]
-      allowable[:cronuts_url]
-    else
-      t('welcome.index.default_cronut_url')
-    end
-
-    @donuts_url = if allowable[:donuts_url]
-      allowable[:donuts_url]
-    else
-      t('welcome.index.default_donut_url')
-    end
+    @cronuts_url = allowable[:cronuts_url] || t('welcome.index.default_cronut_url')
+    @donuts_url  = allowable[:donuts_url]  || t('welcome.index.default_donut_url')
   end
 
   ALLOWABLE = %w[cronuts_url donuts_url].freeze
